80fad53f2319a6620f0dd8fecab4b893357f03e1
Kamran <kmirzaev@gmail.com>
2020-10-07 00:22:42 +0500

    added some javadoc comments

--- src/main/java/com/wine/to/up/recommendation/service/messaging/serialization/EventSerializer.java
+++ src/main/java/com/wine/to/up/recommendation/service/messaging/serialization/EventSerializer.java
@@ -5,7 +5,6 @@ import org.apache.kafka.common.serialization.Serializer;
 
 /**
  * Serializer for {@link KafkaMessageSentEvent}
- * @version 1.0
  */
 public class EventSerializer implements Serializer<KafkaMessageSentEvent> {
     /**
